8bc3fba819b67e347602bedb1258108fbcc89fce
khemmachart.r@apricotion.com <khemmachart.r@apricotion.com>
2013-09-26 17:17:39 +0700

    edit schedule

--- application/controllers/schedule.php
+++ application/controllers/schedule.php
@@ -5,10 +5,10 @@ class Schedule extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		
+
 		//load model
 		$this->load->model('tranfer_model','',TRUE);
-		
+
 		$this->output->set_header('Cache-Control: no-cache, no-store, must-revalidate, max-age=0');
 		$this->output->set_header('Cache-Control: post-check=0, pre-check=0', FALSE);
 		$this->output->set_header('Cache-Control: max-age=-1281, public, must-revalidate, proxy-revalidate', FALSE);
@@ -21,17 +21,17 @@ class Schedule extends CI_Controller {
 
 		$fname =  date('m-d-H-i-s').'.txt';
 		$fh = fopen($cache_path . $fname, 'w');
-		
-	    $list =	$this->tranfer_model->clearBookingData();
+
+	    $list =	array();//$this->tranfer_model->clearBookingData();
 		$str =  "";
 		foreach($list as $o){
-			$str .= json_encode($o)."\n";		
+			$str .= json_encode($o)."\n";
 		}
 
 		//$str =  "schedule task is down by technical";
 		fwrite($fh, $str);
 		fclose($fh);
-		
+
 		$res = array("success"=>true);
 		echo json_encode($res);
 	}
